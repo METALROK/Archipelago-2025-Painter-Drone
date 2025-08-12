28539ec6c1af01781bd825ea0b32f46113e17c93
senpai_sqrt2 <nextikden@gmail.com>
2025-08-12 14:07:39 +0300

    v2.1

--- testing.py
+++ testing.py
@@ -3,6 +3,6 @@ from coordinateGeneration import x_coords, z_coords
 import time
 
 print('Number of points: ', len(x_coords))
-for n in range(len(x_coords) - 2):
+for n in range(len(x_coords) - 1):
     print(x_coords[n+1], z_coords[n+1], '\n')
-    time.sleep(1)
+    #time.sleep(1)
